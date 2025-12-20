e02537c7004a13b53b7574fd70cb6db4e4fc704d
Teshome Mosneh <teshome.mosneh@gmail.com>
2025-12-20 18:04:03 +0300

    create auth schemas

---
+++ src/schemas/aut.schema.js
@@ -0,0 +1,11 @@
+import { z } from "zod";
+const signupSchema = z.object({
+  name: z.string().min(3, "Name should contain at least 3 characters"),
+  email: z.email("Invalid Email Address"),
+  password: z.string().min(4, "Password must be at least 4 characters"),
+});
+const loginSchema = z.object({
+  email: z.email("Invalid Email Address"),
+  password: z.string().min(4, "Password must be at least 4 characters"),
+});
+export { signupSchema, loginSchema };
