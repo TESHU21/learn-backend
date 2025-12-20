a0e4abb91bc605f7e746d91401eab97fcf7346b3
Teshome Mosneh <teshome.mosneh@gmail.com>
2025-12-20 17:50:18 +0300

    Create a validate.js Middleware

---
+++ src/middlewares/validate.js
@@ -0,0 +1,11 @@
+import { zod } from "zod";
+const validateRequest = (schema) => (req, res, next) => {
+  try {
+    schema.parse(req.body); //Throws error if it is invalid
+    next();
+  } catch (error) {
+    // Send the first error message
+    res.status(400).json({ message: error.errors[0].message });
+  }
+};
+export { validateRequest };
