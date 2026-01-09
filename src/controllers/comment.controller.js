d8851d14f85f776c089281f2b2290e119ffe5809
Teshome Mosneh <teshome.mosneh@gmail.com>
2026-01-09 12:59:50 +0300

    add comment controller

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -1,1 +1,55 @@
 import { Comment } from "../models/comment.model";
+import { ApiError } from "../utils/ApiError";
+import { asyncHandler } from "../utils/asyncHandler";
+import { Post } from "../models/post.model";
+import mongoose from "mongoose";
+import { success } from "zod";
+
+export const addComment = asyncHandler(async (req, res) => {
+  const { postId, text } = req.body;
+  const post = await Post.findById(postId);
+  if (!post) {
+    throw new ApiError(404, "Post not found");
+  }
+  //   use transaction(safe update)
+  const session = await mongoose.startSession();
+  session.startTransaction();
+  try {
+    const [comment] = await Comment.create(
+      [
+        {
+          postId,
+          userId: req.user._id,
+          text: text.trim(),
+        },
+      ],
+      { session }
+    );
+    // Increament count automatically
+    await Post.findByIdAndUpdate(
+      postId,
+      { $inc: { commentCount: 1 } },
+      { session }
+    );
+    // commit transaction
+    await session.commitTransaction();
+    // return response with string id
+    res.status(201).json({
+      success: true,
+      message: "Comment Added successfully",
+      data: {
+        id: comment.id,
+        postId: comment.postId.toString(),
+        userId: comment.userId.toString(),
+        text: comment.text,
+        createdAt: comment.createdAt,
+      },
+    });
+  } catch (error) {
+    // rolles back if anything fails
+    await session.abortTransaction();
+    throw error;
+  } finally {
+    session.endSession();
+  }
+});
