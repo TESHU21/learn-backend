5c24a589bb9d25ae094ade77f91de9fe45ada73c
Teshome Mosneh <teshome.mosneh@gmail.com>
2026-01-13 17:34:48 +0300

    fix comment controller

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -33,7 +33,7 @@ const addComment = asyncHandler(async (req, res) => {
   // Increment count
   await Post.findByIdAndUpdate(
     postId,
-    { $inc: { commentsCount: 1 } } // make sure field name matches schema
+    { $inc: { commentCount: 1 } } // make sure field name matches schema
   );
 
   res.status(201).json({
@@ -50,21 +50,35 @@ const addComment = asyncHandler(async (req, res) => {
 });
 const getAllCommentByPost = asyncHandler(async (req, res) => {
   const { postId } = req.params;
-  if (!postId) {
-    throw new ApiError(400, "Post Id are required");
+  const user = req.user;
+  if (!user) {
+    throw new ApiError(404, "USer not found");
   }
+
+  if (!postId) {
+    throw new ApiError(400, "Post Id is required");
+  }
+
+  if (!mongoose.Types.ObjectId.isValid(postId)) {
+    throw new ApiError(400, "Invalid Post Id");
+  }
+
   const comments = await Comment.find({ postId }).populate(
     "userId",
     "_id email role"
   );
-  if (!comments) {
-    throw new ApiError(400, "No Comment Found");
+
+  if (comments.length === 0) {
+    throw new ApiError(404, "No comments found for this post");
   }
+
   res.status(200).json({
-    message: "Comment Retrived successfully",
+    success: true,
+    message: "Comments retrieved successfully",
     data: comments,
   });
 });
+
 // Edit a comment
 const editComment = asyncHandler(async (req, res, next) => {
   const { id } = req.params;
