35625eecc3fe65a61615537154f11f8bbecd0c9d
Teshome Mosneh <teshome.mosneh@gmail.com>
2026-01-22 17:48:22 +0300

    add otp generation and hashing

---
+++ src/models/otp.model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+import crypto from "crypto";
+import { required } from "zod/mini";
+const resetOtpSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    email: { type: String, required: true },
+    otpHash: { type: String, required: true },
+    expiresAt: { type: Date, required: true },
+    attempts: { type: Number, default: 0 },
+    used: { type: Boolean, default: false },
+  },
+  {
+    timestamps: true,
+  }
+);
+resetOtpSchema.methods.verifyOTP = function (otp) {
+  const hash = crypto.createHash("sha256").update(otp).digest("hex");
+  return this.otpHash === hash;
+};
+// TTL Index-auto delete expired OTP
+resetOtpSchema.index({ expiresAt: 1 }, { expireAfterSeconds: 0 });
