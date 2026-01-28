e1be33783bedab923108d4828e87786550d8fced
Teshome Mosneh <teshome.mosneh@gmail.com>
2026-01-28 10:31:34 +0300

    implment http logger

--- src/middlewares/httpLogger.middleware.js
+++ src/middlewares/httpLogger.middleware.js
@@ -1,2 +1,17 @@
-import PinoHttp from "pino-http";
-import logger
+import pinoHttp from "pino-http";
+import logger from "../utils/logger";
+import requestId from "./requestId.middleware";
+const httpLogger = pinoHttp({
+  logger,
+  customProps: (req, res) => ({
+    requestId: req.requestId,
+    ip: req.headers["x-forwarded-for"] || req.socket.remoteAddress,
+    userId: req.user._id || "ananymous",
+    endpoints: `${req.method} ${req.originalUrl}`,
+  }),
+  customSuccessMessage: (req, res) =>
+    `${req.method} ${req.originalUrl} completed`,
+  customErrorMessage: (req, res, err) =>
+    `${req.method} ${req.originalUrl} failed`,
+});
+export default httpLogger;
