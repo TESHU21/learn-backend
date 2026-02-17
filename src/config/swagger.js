a95d187b1ce0f09063a4a86381affe3bedbb1e91
Teshome Mosneh <teshome.mosneh@gmail.com>
2026-02-17 08:34:17 +0300

    add  reusable componens of swagger.js configuration

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -2,50 +2,107 @@ import swaggerJSDoc from "swagger-jsdoc";
 
 const options = {
   definition: {
-    openapi: "3.0.0", // Swagger version
+    openapi: "3.0.0",
     info: {
       title: "Learn Backend API",
       version: "1.0.0",
       description: "Professional API documentation using Swagger",
     },
+
     servers: [
       {
-        url: "http://localhost:4000", // Your backend base URL
+        url: "http://localhost:4000",
       },
     ],
+
     components: {
       securitySchemes: {
-        // JWT support
         bearerAuth: {
           type: "http",
           scheme: "bearer",
           bearerFormat: "JWT",
         },
       },
+
       schemas: {
-        // Reusable objects
         Post: {
           type: "object",
+          required: ["name", "description", "age"],
           properties: {
-            name: { type: "string", example: "My first post" },
-            description: { type: "string", example: "This is a post" },
-            age: { type: "number", example: 25 },
-            author: { type: "string", example: "userId123" },
-            status: { type: "string", example: "pending" },
+            name: {
+              type: "string",
+              example: "My first post",
+            },
+            description: {
+              type: "string",
+              example: "This is a post",
+            },
+            age: {
+              type: "number",
+              example: 25,
+            },
+            author: {
+              type: "string",
+              example: "userId123",
+            },
+            status: {
+              type: "string",
+              example: "pending",
+            },
           },
         },
+
         User: {
           type: "object",
           properties: {
-            username: { type: "string", example: "Teshome" },
-            email: { type: "string", example: "teshome@example.com" },
-            role: { type: "string", example: "admin" },
+            username: {
+              type: "string",
+              example: "Teshome",
+            },
+            email: {
+              type: "string",
+              example: "teshome@example.com",
+            },
+            password: {
+              type: "string",
+              example: "1234567",
+            },
+
+            role: {
+              type: "string",
+              example: "admin",
+            },
+          },
+        },
+
+        Comment: {
+          type: "object",
+          required: ["text"],
+          properties: {
+            text: {
+              type: "string",
+              example: "Good Boy!",
+            },
+          },
+        },
+        Reaction: {
+          type: "object",
+          required: ["type"],
+          properties: {
+            type: {
+              type: "string",
+              description: "Reaction type for a post",
+              enum: ["like", "love", "haha", "wow", "sad", "angry"],
+              example: "love",
+            },
           },
         },
       },
     },
-    security: [{ bearerAuth: [] }], // Global JWT protection
+
+    security: [{ bearerAuth: [] }],
   },
+
   apis: ["./src/docs/**/*.js"],
 };
 
